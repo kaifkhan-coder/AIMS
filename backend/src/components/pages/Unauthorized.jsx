918d066999f16e6c091d9333198bfdd61e757bf8
kaifkhan-coder <khankaif43058@gmail.com>
2026-03-19 20:52:15 +0530

    fix: remove wrongly placed src folder from backend

--- backend/src/components/pages/Unauthorized.jsx
+++
@@ -1,9 +0,0 @@
-const Unauthorized = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center text-xl">
-      ❌ You are not authorized to access this page
-    </div>
-  );
-};
-
-export default Unauthorized;
--- backend/src/services/authService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const API = `${import.meta.env.VITE_API_URL}/api/auth`;
-
-export const registerUser = (data) =>
-  axios.post(`${API}/register`, data);
-
-export const loginUser = (data) =>
-  axios.post(`${API}/login`, data);
