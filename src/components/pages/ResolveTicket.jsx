54536686ae16484192012aafb13f1d47749b3c57
kaifkhan-coder <khankaif43058@gmail.com>
2026-04-12 21:27:34 +0530

    fix: resolve import path

--- src/components/pages/ResolveTicket.jsx
+++ src/components/pages/ResolveTicket.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import api from "../services/api";
+// import api from "../services/api";
+import api from "../../services/api";
 
 export default function ResolveTicket() {
   const { id } = useParams();
