0089e7bc4b047ada903010a5a7906a72395fdb42
kaifkhan-coder <khankaif43058@gmail.com>
2026-04-14 21:13:48 +0530

    Initial clean kaif7 commit'

--- src/App.jsx
+++ src/App.jsx
@@ -198,7 +198,7 @@ function App() {
           <Route path="/unauthorized" element={<Unauthorized />} />
           <Route path="/verify/:staffId" element={<VerifyStaffPage />} />
 <Route path="/resolve/:id" element={<ResolveTicket />} />
-<Route path="/resolve-ticket/:id" element={<ResolveTicket />} />
+{/* <Route path="/resolve-ticket/:id" element={<ResolveTicket />} /> */}
           <Route path="/support" element={<Support />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
